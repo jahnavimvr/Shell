abaa4d4919c8a1e66bfd45c9326a412ee21a2981
jahnavimvr <jahnavimvr@gmail.com>
2025-07-27 19:59:37 +0530

    added one line for fork

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+i#!/bin/bash
 ################################
 # Author: Abhishek
 # Version: v1
@@ -8,7 +8,7 @@
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
-#
+# added one line for git fork
 #
 ################################
 
